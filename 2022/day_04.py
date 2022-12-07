64866283fcc358552a4d98ce21441ba7b65fd0b3
eliwjones <eli.jones@gmail.com>
2022-12-07 14:38:19 -0800

    day_04.py: now deals with overlapping_ranges().

--- 2022/day_04.py
+++ 2022/day_04.py
@@ -1,32 +1,32 @@
 import unittest
 
 
-def contained_ranges(str_data):
+def overlapping_ranges(str_data):
     parsed_data = str_data.replace('-', '\n').replace(',', '\n').split('\n')
 
     pairs = [int(item) for item in parsed_data]
-    contained = []
+    overlapping = []
     for i in range(0, len(pairs), 4):
         range_one = set(range(pairs[i], pairs[i + 1] + 1))
         range_two = set(range(pairs[i + 2], pairs[i + 3] + 1))
 
         intersection = range_one & range_two
-        if len(intersection) < len(range_one) and len(intersection) < len(range_two):
+        if not intersection:
             continue
 
-        contained.append([range_one, range_two])
+        overlapping.append([range_one, range_two])
 
-    return len(contained)
+    return len(overlapping)
 
 
 class Test(unittest.TestCase):
-    def test_contained_ranges(self):
+    def test_overlapping_ranges(self):
         data = ['2-4,6-8', '2-3,4-5', '5-7,7-9', '2-8,3-7', '6-6,4-6', '2-6,4-8']
         str_data = '\n'.join(map(str, data))
 
-        contained = contained_ranges(str_data)
+        overlapping = overlapping_ranges(str_data)
 
-        self.assertEqual(contained, 2, f"Expected contained ranges to be 2 but got {contained}.")
+        self.assertEqual(overlapping, 4, f"Expected overlapping ranges to be 4 but got {overlapping}.")
 
 
 if __name__ == '__main__':
