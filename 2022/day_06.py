3338ce865f732286a42649828687ced758ca4713
eliwjones <eli.jones@gmail.com>
2022-12-07 14:56:33 -0800

    day_06.py: start_marker() - handle message marker as well as packet marker.

--- 2022/day_06.py
+++ 2022/day_06.py
@@ -1,9 +1,9 @@
 import unittest
 
 
-def start_marker(str_buffer):
-    for i in range(4, len(str_buffer) + 1):
-        marker = str_buffer[i - 4 : i]
+def start_marker(str_buffer, length):
+    for i in range(length, len(str_buffer) + 1):
+        marker = str_buffer[i - length : i]
         if len(marker) == len(set(marker)):
             return i, marker
 
@@ -13,20 +13,24 @@ def start_marker(str_buffer):
 class Test(unittest.TestCase):
     def test_start_marker(self):
         test_cases = [
-            ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7),
-            ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5),
-            ('nppdvjthqldpwncqszvftbrmjlhg', 6),
-            ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10),
-            ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11),
-            ('1111111111111111111112345', 24),
-            ('', -1),
-            ('aaaaaaaaaaaaaaaaaaaaaaaaa', -1),
+            ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7, 19),
+            ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5, 23),
+            ('nppdvjthqldpwncqszvftbrmjlhg', 6, 23),
+            ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10, 29),
+            ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11, 26),
+            ('1111111111111111111112345', 24, -1),
+            ('', -1, -1),
+            ('aaaaaaaaaaaaaaaaaaaaaaaaa', -1, -1),
         ]
 
-        for str_buffer, expected in test_cases:
-            index, marker = start_marker(str_buffer)
+        for str_buffer, packet_marker, message_marker in test_cases:
+            index, marker = start_marker(str_buffer, 4)
 
-            self.assertEqual(index, expected, f"Expected start_marker to be {expected} but got {index}.")
+            self.assertEqual(index, packet_marker, f"Expected start packet marker to be {packet_marker} but got {index}.")
+
+            index, marker = start_marker(str_buffer, 14)
+
+            self.assertEqual(index, message_marker, f"Expected start message marker to be {message_marker} but got {index}.")
 
 
 if __name__ == '__main__':
