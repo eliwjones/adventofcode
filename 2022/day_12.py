5b1de59e1a7eeea9868b16dae12dca97dfda139b
eliwjones <eli.jones@gmail.com>
2022-12-13 12:40:29 -0800

    Day 12: Part 1

--- 2022/day_12.py
+++ 2022/day_12.py
@@ -40,27 +40,59 @@ def process_maze(maze):
 
 
 def find_paths(moves, start, end):
+    from copy import deepcopy
+
     winners = []
-    paths = [[start]]
+    paths = [{start: None}]
+
+    visited = {}
+
     while paths:
         new_paths = []
         for path in paths:
-            pos = path[-1]
+            pos = list(path.keys())[-1]
 
             if pos == end:
                 winners.append(path)
                 continue
 
-            valid_moves = [(i, j) for i, j in moves[pos[0]][pos[1]] if (i, j) not in path]
+            seen = path | visited
+            valid_moves = [(i, j) for i, j in moves[pos[0]][pos[1]] if (i, j) not in seen]
             if not valid_moves:
                 continue
 
             for i, j in valid_moves:
-                new_paths.append(path + [(i, j)])
+                new_path = deepcopy(path)
+                new_path[(i, j)] = None
+                visited[(i, j)] = None
+
+                new_paths.append(new_path)
 
         paths = new_paths
 
     return winners
+
+
+def manhattan(one, two):
+    d = abs(one[0] - two[0]) + abs(one[1] - two[1])
+
+    return d
+
+
+def trim_moves(valid_moves, pos, end):
+    """
+    Not used! But kept around just in case.
+    """
+    weights = []
+    for m in valid_moves:
+        move_pos = (pos[0] + m[0], pos[1] + m[1])
+        weight = manhattan(move_pos, end)
+
+        weights.append((weight, m))
+
+    min_weight = min(w[0] for w in weights)
+
+    return [w[1] for w in weights if w[0] == min_weight]
 
 
 class Test(unittest.TestCase):
@@ -69,23 +101,10 @@ class Test(unittest.TestCase):
         maze = [[*line] for line in data]
 
         moves, start, end, maze = process_maze(maze)
-
-        print(f"\nstart: {start}, end: {end}")
-
-        ijs = [(i, j) for i in range(len(moves)) for j in range(len(moves[i]))]
-        for i, j in ijs:
-            if end not in moves[i][j]:
-                continue
-            print(f"moves[{i}][{j}]: {moves[i][j]}")
-
         winners = find_paths(moves, start, end)
 
         min_len = min([len(winner) for winner in winners]) - 1
         expected = 31
-
-        for winner in [w for w in winners if len(w) == min_len]:
-            print("***************** winner ****************")
-            print(winner)
 
         self.assertEqual(min_len, expected, f"Expected min length to be {expected} but got {min_len}.")
 
