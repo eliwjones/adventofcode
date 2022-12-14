8fafd8fca6c8e01f2395739fca463923d8efae85
eliwjones <eli.jones@gmail.com>
2022-12-14 14:42:52 -0800

    Day 14: Part 1

--- 2022/day_14.py
+++ 2022/day_14.py
@@ -2,26 +2,37 @@ import unittest
 
 ROCK = '#'
 SAND = 'o'
+AIR = '.'
 
 
 def simulate(cave, sand_source):
-    """
-    Simulate falling sand until grains begin falling past
-    the "floor".
-    """
-
-    """
-      If sand gets to edge of cave, simulation is over
-      next move would fall off.
-    """
-    print('\n')
-    for row in cave:
-        print(''.join(row[-50:]))
+    sand_moves = ((0, 1), (-1, 1), (1, 1))
 
     while True:
-        print(sand_source)
+        pos = list(sand_source)
 
-        break
+        while True:
+            moved = False
+            for x, y in sand_moves:
+                i = pos[1] + y
+                j = pos[0] + x
+
+                if not (0 <= i < len(cave)) or not (0 <= j < len(cave[i])):
+                    return cave
+
+                if cave[i][j] != AIR:
+                    continue
+
+                pos = [j, i]
+                moved = True
+
+                break
+
+            if not moved:
+                cave[pos[1]][pos[0]] = SAND
+                print_cave(cave)
+
+                break
 
     return cave
 
@@ -30,7 +41,7 @@ def build_cave(paths):
     max_x = max([paths[i][j][0] for i, j in ijs(paths)])
     max_y = max([paths[i][j][1] for i, j in ijs(paths)])
 
-    cave = [['.' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+    cave = [[AIR for _ in range(max_x + 1)] for _ in range(max_y + 1)]
     for path, i in [(path, i) for path in paths for i in range(len(path) - 1)]:
         one, two = sort_points(path[i], path[i + 1])
 
@@ -68,6 +79,12 @@ def parse_path_data(str_data):
     return paths
 
 
+def print_cave(cave):
+    print('\n')
+    for row in cave:
+        print(''.join(row[-50:]))
+
+
 def sort_points(one, two):
     if one[0] < two[0]:
         return one, two
@@ -89,8 +106,12 @@ class Test(unittest.TestCase):
         print()
 
         paths = parse_path_data(str_data)
-        cave = simulate(cave=build_cave(paths), sand_source=(500, 0))
-        sand_count = count_things(cave=cave, thing='o')
+        cave = build_cave(paths)
+
+        print_cave(cave)
+
+        cave = simulate(cave=cave, sand_source=(500, 0))
+        sand_count = count_things(cave=cave, thing=SAND)
         expected = 24
 
         self.assertEqual(sand_count, expected, f"Expected to see {expected} grains of sand but got {sand_count}.")
